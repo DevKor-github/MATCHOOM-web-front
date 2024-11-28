79d9632ee977158246aa705cd0e7f805e739cad5
Geon <geonulim@gmail.com>
2024-11-28 15:37:53 +0900

    feat: 결제 규정 바텀시트 기초 구현

--- src/pages/PointPage.tsx
+++ src/pages/PointPage.tsx
@@ -1,29 +1,41 @@
+import useBottomSheet from 'hooks/useBottomSheet';
+import BottomSheet from 'components/common/bottom-sheet/BottomSheet';
+
 const PointPage = () => {
+  const { bottomSheetRef, contentRef, openBottomSheet } = useBottomSheet();
+
   return (
-    <div>
-      <header>지갑</header>
-      <main className='flex flex-col gap-20 p-20'>
-        <div>
-          <span>JUST JERK ACADEMY</span>
-          <button>결제규정</button>
-          <button>결제내역</button>
-        </div>
-        <div>
-          <span>권태현님</span>
+    <>
+      <div>
+        <header>지갑</header>
+        <main className='flex flex-col gap-20 p-20'>
+          <div>
+            <span>JUST JERK ACADEMY</span>
+            <button onClick={() => openBottomSheet()}>결제규정</button>
+            <button>결제내역</button>
+          </div>
+          <div>
+            <span>권태현님</span>
+            <ul>
+              <MyPointItem point={30000} expiredAt='24.12.03' />
+              <MyPointItem point={20000} expiredAt='24.12.03' />
+              <MyPointItem point={10000} expiredAt='24.12.03' />
+            </ul>
+          </div>
           <ul>
-            <MyPointItem point={30000} expiredAt='24.12.03' />
-            <MyPointItem point={20000} expiredAt='24.12.03' />
-            <MyPointItem point={10000} expiredAt='24.12.03' />
+            <PointItem point={10000} price={10000} />
+            <PointItem point={20000} price={20000} />
+            <PointItem point={30000} price={30000} />
           </ul>
+          <button>결제하기</button>
+        </main>
+      </div>
+      <BottomSheet ref={bottomSheetRef} title={'결제규정'}>
+        <div ref={contentRef} className='flex flex-col gap-12'>
+          결제 규정입니다
         </div>
-        <ul>
-          <PointItem point={10000} price={10000} />
-          <PointItem point={20000} price={20000} />
-          <PointItem point={30000} price={30000} />
-        </ul>
-        <button>결제하기</button>
-      </main>
-    </div>
+      </BottomSheet>
+    </>
   );
 };
 
